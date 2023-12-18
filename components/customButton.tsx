b1fb908fda3e594de2c658fc31dbadd98b651785
halilibrahimdemircan <58706397+halilibrahimdemircan@users.noreply.github.com>
2023-12-18 17:31:44 +0300

    modal

---
+++ components/customButton.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Cinzel } from "next/font/google";
+
+type Props = {
+  text: string;
+  bg: string;
+  link?: string;
+  action?: (() => void) | (() => Promise<void>);
+};
+const cinzel = Cinzel({ subsets: ["latin"] });
+const CustomButton = ({ text, bg, link, action }: Props) => {
+  return (
+    <div>
+      {text == "line" ? (
+        <hr className="my-3 mx-20"></hr>
+      ) : (
+        <button
+          className={`${bg} ${cinzel.className} text-[18px] leading-4 tracking-normal text-center w-full px-3 py-4 font-medium shadow-sm text-white my-2 rounded-md `}
+        >
+          <a target="_blank" href={link}>
+            {text}
+          </a>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default CustomButton;
--- pages/landofmush/index.tsx
+++ pages/landofmush/index.tsx
@@ -1,7 +1,45 @@
 import type { NextPage } from "next";
+import { useState } from "react";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Modal from "../../components/modal";
+import Image from "next/image";
+import lomBrand from "../../public/lomBrand.png";
 
 const Homepage: NextPage = () => {
-  return <div className="border text-red-500"></div>;
+  const [open, setOpen] = useState(true);
+
+  const notify = () =>
+    toast.info("YOU HAVE SUCCESSFULLY JOINED THE WHITELIST!");
+  return (
+    <div>
+      <Modal open={open} setOpen={setOpen} />
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      {/* <button onClick={notify}>JOIN</button> */}
+      <div className="flex w-full justify-between px-12 py-8">
+        <div className="">
+          <Image
+            alt="Land of Mush Icon"
+            src={lomBrand}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        </div>
+        <div>icons</div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
