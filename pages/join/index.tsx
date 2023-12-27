4e541f8e71d72f0a4f96859e41215f273f98df43
halilibrahimdemircan <58706397+halilibrahimdemircan@users.noreply.github.com>
2023-12-27 22:41:25 +0300

    added full screen button

--- pages/join/index.tsx
+++ pages/join/index.tsx
@@ -1,12 +1,20 @@
 import type { NextPage } from "next";
-import { useRef } from "react";
+import Image from "next/image";
+import fullScreen from "../../public/fullScreen.png";
 
 const Joinpage: NextPage = () => {
+  const toggleFullScreen = () => {
+    const iframe = document.getElementById("gameFrame");
+
+    if (iframe) {
+      iframe.requestFullscreen();
+    }
+  };
   return (
     <div className="flex justify-center items-center h-full relative">
       <iframe
         className="w-[962px] h-[640px] rounded"
-        id="game_drop"
+        id="gameFrame"
         // src="https://html-classic.itch.zone/html/9338311/webgl0x3a/index.html"
         src="https://html-classic.itch.zone/html/9384198/webl05/index.html"
         // frameBorder="0"
@@ -18,6 +26,18 @@ const Joinpage: NextPage = () => {
         // msallowfullscreen={true}
         // allow="autoplay; fullscreen *; geolocation; microphone; camera; midi; monetization; xr-spatial-tracking; gamepad; gyroscope; accelerometer; xr; cross-origin-isolated"
       ></iframe>
+      <div className="h-full p-6">
+        <div
+          onClick={toggleFullScreen}
+          className={` bg-gray-700  rounded-lg h-12 flex items-center px-[18px] cursor-pointer`}
+        >
+          <Image
+            alt="Land of Mush Icon"
+            src={fullScreen}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        </div>
+      </div>
     </div>
   );
 };
